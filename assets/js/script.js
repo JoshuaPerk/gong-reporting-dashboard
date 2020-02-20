205443d421cc41deab4f0d9a139042575a6dd9ce
Joshua Perk <joshrsperk@gmail.com>
2020-02-20 10:21:09 -0500

    Added workspaceId to path

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@ let jsonGongData, filterResult, team;
 let labels, data, filterArray = [];
 let baseURL = "https://app.gong.io/calls/ajax/calls?";
 let companyId = "3568831574419877865";
+let workspaceId = "2051577397987143168";
 let pageSize = 5000;
 let pageOffset = 0;
 var CLIENT_ID = '393986627865-du66b48n8dtc1iscfnqh4olp1qp47otk.apps.googleusercontent.com';
@@ -68,7 +69,7 @@ $('#generateURL').click(function(){
   if (filterArray.length) {
     filterResult = `"search":{"type":"And","filters":[${filterArray}]}`;
   }
-  $("#gongURL").val(`https://app.gong.io/calls/ajax/calls?company-id=${companyId}&pageSize=${pageSize}&offset=${pageOffset}&callSearch={${filterResult}}`);
+  $("#gongURL").val(`${baseURL}?company-id=${companyId}?workspace-id=${workspaceId}&pageSize=${pageSize}&offset=${pageOffset}&callSearch={${filterResult}}`);
     $("#gongURL").select();
     document.execCommand("copy");
     window.open(`https://app.gong.io/calls/ajax/calls?company-id=${companyId}&pageSize=${pageSize}&offset=${pageOffset}&callSearch={${filterResult}}`);
